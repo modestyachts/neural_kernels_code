76e8005168d71b0924ee12e235e6f9bcfc1386bb
Vaishaal Shankar <vaishaal@berkeley.edu>
2020-02-18 07:35:54 +0000

    myrtle5 typo

--- cifar10/run_kernel_myrtle5.sh
+++ cifar10/run_kernel_myrtle5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ulimit -n 99999
 pushd ..
-python run_train_eval_exp.py --config configs/myrtle5.yaml   --config-updates DATASET.TRAIN_SUBSET 50000 DATASET.TEST_SUBSET 50000 DATASET.NAME  "cifar-10"  SYSTEM.BATCH_SIZE 48
+python run_train_eval_exp.py --config configs/myrtle5.yaml   --config-updates DATASET.TRAIN_SUBSET 50000 DATASET.TEST_SUBSET 10000 DATASET.NAME  "cifar-10"  SYSTEM.BATCH_SIZE 48
