5f0289d9d4b927a6b288bbe146514f5074fcf15a
Vaishaal Shankar <vaishaal@berkeley.edu>
2020-02-18 07:18:39 +0000

    run kernel myrtle5 subsampled

---
+++ subsampled_cifar10/run_kernel_myrtle5_subsampled.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ulimit -n 99999
+pushd ..
+python run_train_eval_exp.py --config configs/myrtle5.yaml   --config-updates DATASET.TRAIN_SUBSET $1 DATASET.TEST_SUBSET 10000  DATASET.NAME  "cifar-10"  SYSTEM.BATCH_SIZE 48
